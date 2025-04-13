19f83aabc7624e5f8a0b2a04435041e076d38908
Andrea Graziano Gitto <andreagraziano.g33personale@gmail.com>
2025-04-13 14:07:28 +0000

    finished

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 import demographic_data_analyzer
 from unittest import main
 
+
 # Test your function by calling it here
+
+
 demographic_data_analyzer.calculate_demographic_data()
 
 # Run unit tests automatically
